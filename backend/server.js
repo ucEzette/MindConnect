3559d37a002c869a30bc82c8acd306e1a35a4809
ucEzette <u.ezette@alustudent.com>
2025-11-06 02:02:04 +0200

    feat: Add authentication middleware and server setup

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+require('dotenv').config();
